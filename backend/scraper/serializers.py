bc95a91d6551384b8bff2482d52bfe44b1043fc8
alrxkali <accual1013@gmail.com>
2022-07-30 23:58:10 -0400

    GET and POST endpoints ready

---
+++ backend/scraper/serializers.py
@@ -0,0 +1,8 @@
+from tkinter import SE
+from rest_framework import serializers
+from .models import Search_Param
+
+class Search_ParamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Search_Param
+        fields = ['search_term', 'quantity']
--- backend/scraper/views.py
+++ backend/scraper/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http.response import JsonResponse
+
+from .models import Search_Param as API
+from .serializers import Search_ParamSerializer
 
 # Create your views here.
+# def SearchAPI(request):
+#     search = API.objects.all()
+#     searchSerializer = Search_ParamSerializer(search, many=True)
+#     return JsonResponse(searchSerializer.data, safe=False)
+@api_view(['GET', 'POST'])
+def SearchAPI(request):
+    if request.method == 'GET':
+        search = API.objects.all()
+        searchSerializer = Search_ParamSerializer(search, many=True)
+        return JsonResponse(searchSerializer.data, safe=False)
+
+    if request.method == 'POST':
+        searchSerializer = Search_ParamSerializer(data=request.data)
+        if searchSerializer.is_valid():
+            searchSerializer.save()
+            return Response(searchSerializer.data, status=status.HTTP_201_CREATED)
+
+    # elif request.method == 'POST':
+    #     search_data = JSONParser().parse(request)
+    #     search_serializer = Search_ParamSerializer(data=search_data)
+    #     if search_serializer.is_valid():
+    #         search_serializer.save()
+    #         return JsonResponse("Added Successfully!", safe=False)
+    #     return JsonResponse("Failed to Add.", safe=False)
+
